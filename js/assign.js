e1780b5c0b4837c977077d27512ff764f99cb9b1
hexf00 <hexf00@gmail.com>
2020-02-13 23:25:49 +0800

    docs: 补充assign合并对象细节

---
+++ js/assign.js
@@ -0,0 +1,8 @@
+console.log(Object.assign({ a: { a: 1 } }, { a: { b: 1 } })); //输出{ a: { b: 1 } }
+
+var a = { a: { a: 1 } }
+var b = { a: { b: 1 } }
+var c = Object.assign({}, a, b);
+c.a.b = 2;
+console.log(a,b,c);//{ a: { a: 1 } } { a: { b: 2 } } { a: { b: 2 } }
+
