4ff4f20215286d718be4824536237ab91bb2ca48
hexf00 <hexf00@gmail.com>
2020-02-14 21:12:42 +0800

    [docs] 添加关于流程管理的思考

---
+++ js/task.js
@@ -0,0 +1,24 @@
+// page = page + 1;
+
+
+// _old = page;
+// page = 1;
+
+// getData().error(error => {
+//     page = _old;
+//     return error
+// })
+
+// getData().error(error => {
+//     page = page - 1;
+//     return error
+// })
+
+
+// getData({
+//     error: error => {
+//         page = page - 1;
+//         return error
+//     }
+// })
+
